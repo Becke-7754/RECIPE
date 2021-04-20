2d310c357a011f94f01ab9c7a0d0fd2ce03e8f03
becke7754@gmail.com <62795043+Becke-7754@users.noreply.github.com>
2021-04-21 07:17:38 +0800

    Huudaslalt bolon search modul implement hiiw

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -26,11 +26,50 @@ export const clearSearchQuerry = () => {
 
 export const clearSearchResult = () => {
     elements.searchResultList.innerHTML = '';
+    elements.pageButtons.innerHTML = '';
 };
 
 export const getInput = () => elements.searchInput.value;
-export const renderRecipes = recipes => {
-    recipes.forEach(renderRecipe);
-}
+export const renderRecipes = (recipes, currentPage = 1, resPerPage = 10) => {
 
+// Хайлтын үр дүнг хуудаслаж үзүүлэх
+    // page = 2, start = 10, end = 20
+    const start = (currentPage - 1) * resPerPage;
+    const end = currentPage * resPerPage;
+    
+        recipes.slice(start, end).forEach(renderRecipe);
+
+// хуудаслалтын товчуудыг гаргаж ирэх
+const totalPages = Math.ceil(recipes.length / resPerPage );
+renderButtons(currentPage, totalPages);
+
+};
+
+const createButton = (page, type, direction) => `<button class="btn-inline results__btn--${type}" data-goto=${page}>
+<svg class="search__icon">
+    <use href="img/icons.svg#icon-triangle-${direction}"></use>
+</svg>
+<span>Хуудас ${page}</span>
+</button>`;
+
+
+const renderButtons = (currentPage, totalPages) => {
+    let buttonHtml ;
+
+    if(currentPage === 1 && totalPages > 1 ) {
+        // 1-р хуудсан дээр байна, 2-р хуудас гэдэг товчийг гаргана.
+        buttonHtml = createButton(2, "next", "right");
+    } else if (currentPage < totalPages) {
+        // Өмнөх болон дараагийн хуудасруу шилжих  товчийг үзүүлэх.
+        buttonHtml = createButton(currentPage - 1, "prev", "left");
+        buttonHtml += createButton(currentPage + 1, "next", "right");
+    }
+    else if (currentPage === totalPages) {
+        // Хамгийн сүүлийн хуудас дээр байна. Өмнөх рүү шилжүүлэх товчийг  л үзүүлнэ.
+        buttonHtml = createButton(currentPage - 1,"prev", "left")
+    }
+
+
+    elements.pageButtons.insertAdjacentHTML('afterbegin', buttonHtml);
+};
 
